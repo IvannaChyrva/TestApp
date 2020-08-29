45a4eaeebcead546991d87dcf573b730320468df
ivankachirva <ivankachirva@gmail.com>
2020-08-29 16:22:12 +0300

    Added a few records to see pagination

--- TestApp/InitialData.cs
+++ TestApp/InitialData.cs
@@ -53,6 +53,70 @@ namespace TestApp
                         PhoneNumber = "+380-988-456-325",
                         Email = "mikhailoailo@gmail.com",
                         FacilityStatus = context.FacilityStatuses.Where(f => f.Name == "OnHold").FirstOrDefault()
+                    },
+                    new Facility
+                    {
+                        Name = "Grisha",
+                        Address = "Verbytskogo 9, 85, Kyiv",
+                        PhoneNumber = "+380-589-788-123",
+                        Email = "gggrisha@gmail.com",
+                        FacilityStatus = context.FacilityStatuses.Where(f => f.Name == "Inactive").FirstOrDefault()
+                    },
+                    new Facility
+                    {
+                        Name = "Mila",
+                        Address = "Bandery 5, 106, Oslo",
+                        PhoneNumber = "+380-989-625-767",
+                        Email = "milagamnila@gmail.com",
+                        FacilityStatus = context.FacilityStatuses.Where(f => f.Name == "OnHold").FirstOrDefault()
+                    },
+                    new Facility
+                    {
+                        Name = "Yulia",
+                        Address = "Shevchenka 85, 57, Kyiv",
+                        PhoneNumber = "+380-668-771-254",
+                        Email = "yuliagrusi@gmail.com",
+                        FacilityStatus = context.FacilityStatuses.Where(f => f.Name == "Inactive").FirstOrDefault()
+                    },
+                    new Facility
+                    {
+                        Name = "Oleg",
+                        Address = "Bandery 1, 6, Oslo",
+                        PhoneNumber = "+380-896-456-115",
+                        Email = "olegjikl@gmail.com",
+                        FacilityStatus = context.FacilityStatuses.Where(f => f.Name == "OnHold").FirstOrDefault()
+                    },
+                    new Facility
+                    {
+                        Name = "Nazar",
+                        Address = "Shevchenka 75, 311, Kyiv",
+                        PhoneNumber = "+380-683-158-111",
+                        Email = "nazarnna@gmail.com",
+                        FacilityStatus = context.FacilityStatuses.Where(f => f.Name == "Inactive").FirstOrDefault()
+                    },
+                    new Facility
+                    {
+                        Name = "Mila",
+                        Address = "Bandery 1, 15, Oslo",
+                        PhoneNumber = "+380-975-450-300",
+                        Email = "milamila@gmail.com",
+                        FacilityStatus = context.FacilityStatuses.Where(f => f.Name == "OnHold").FirstOrDefault()
+                    },
+                    new Facility
+                    {
+                        Name = "Valerii",
+                        Address = "Shevchenka 93, 11, Kyiv",
+                        PhoneNumber = "+380-689-141-131",
+                        Email = "valerii1265@gmail.com",
+                        FacilityStatus = context.FacilityStatuses.Where(f => f.Name == "Inactive").FirstOrDefault()
+                    },
+                    new Facility
+                    {
+                        Name = "Kira",
+                        Address = "Kochevnuka 32, 20, Lviv",
+                        PhoneNumber = "+380-985-256-245",
+                        Email = "125kira125@gmail.com",
+                        FacilityStatus = context.FacilityStatuses.Where(f => f.Name == "OnHold").FirstOrDefault()
                     }
                     );
 
